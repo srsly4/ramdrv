e3a7a41bc30b50960805e3e8389db31ffed744ea
Szymon Piechaczek <piechaczek.szymon@gmail.com>
2017-06-11 13:32:36 +0200

    Bio request handling

--- kmod/ramdrv.c
+++ kmod/ramdrv.c
@@ -14,6 +14,7 @@
 #include <linux/blkdev.h>
 #include <linux/genhd.h>
 #include <linux/proc_fs.h>
+#include <linux/bio.h>
 
 #include "ramdrv_internal.h"
 #include "../include/linux/ramdrv.h"
@@ -27,6 +28,15 @@ module_param(sector_count, int, 0);
 
 // block device major number
 static int blkdev_id;
+
+static int bytes_to_sectors_checked(unsigned long bytes)
+{
+	if( bytes % KERNEL_SECTOR_SIZE )
+	{
+		printk("***************WhatTheFuck***********************\n");
+	}
+	return bytes / KERNEL_SECTOR_SIZE;
+}
 
 static int sbull_open(struct block_device *dev, fmode_t mode){
   struct sbull_dev *sdev;
@@ -66,10 +76,10 @@ static int sbull_ioctl(struct block_device *dev, fmode_t mode,
                         unsigned cmd, unsigned long arg){
   //struct sbull_dev* sdev = dev->bd_disk->private_data;
   struct hd_geometry disk_geometry;
-  printk("ramdrv: ioctl command!\n");
+  printk(KERN_NOTICE "ramdrv: ioctl command!\n");
   switch(cmd){
     case HDIO_GETGEO:
-    printk("ramdrv: getting geometry info\n");
+    printk(KERN_NOTICE "ramdrv: getting geometry info\n");
     disk_geometry.cylinders = 512;
     disk_geometry.heads = 4;
     disk_geometry.sectors = 16;
@@ -98,7 +108,85 @@ static void sbull_transfer(struct sbull_dev *dev, unsigned long sector,
     memcpy(buffer, dev->data + offset, nbytes);
 }
 
-// Device request handler
+
+static int sbull_xfer_bio(struct sbull_dev *dev, struct bio *bio)
+{
+	struct bio_vec bvec;
+	struct bvec_iter iter;
+	sector_t sector = bio->bi_iter.bi_sector;
+
+	/* Do each segment independently. */
+	bio_for_each_segment(bvec, bio, iter) {
+		char *buffer = __bio_kmap_atomic(bio, iter);
+		sbull_transfer(dev, sector,bytes_to_sectors_checked(bio_cur_bytes(bio)),
+				buffer, bio_data_dir(bio) == WRITE);
+		sector += (bytes_to_sectors_checked(bio_cur_bytes(bio)));
+		__bio_kunmap_atomic(bio);
+	}
+	return 0; /* Always "succeed" */
+}
+
+
+/*
+ * Transfer a full request.
+ */
+static int sbull_xfer_request(struct sbull_dev *dev, struct request *req)
+{
+	struct bio *bio;
+	int nsect = 0;
+
+	__rq_for_each_bio(bio, req) {
+		sbull_xfer_bio(dev, bio);
+		nsect += bio->bi_iter.bi_size/KERNEL_SECTOR_SIZE;
+	}
+	return nsect;
+}
+
+
+
+/*
+ * Smarter request function that "handles clustering".
+ */
+static void sbull_full_request(struct request_queue *q)
+{
+	struct request *req;
+	struct sbull_dev *dev = q->queuedata;
+	int ret;
+
+	while ((req = blk_fetch_request(q)) != NULL) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,0,0)
+    if (req == NULL || (!(req->cmd_flags & REQ_OP_WRITE) && !(req->cmd_flags & REQ_OP_READ))) { //if it's not from-disk or to-disk request
+#else
+    if (req == NULL || req->cmd_type != REQ_TYPE_FS) {
+#endif
+			printk (KERN_NOTICE "Skip non-fs request\n");
+			ret = -EIO;
+			goto done;
+		}
+		sbull_xfer_request(dev, req);
+		ret = 0;
+	done:
+		__blk_end_request_all(req, ret);
+	}
+}
+
+
+
+/*
+ * The direct make request version.
+ */
+static blk_qc_t sbull_make_request(struct request_queue *q, struct bio *bio)
+{
+	struct sbull_dev *dev = q->queuedata;
+	int status;
+
+	status = sbull_xfer_bio(dev, bio);
+	bio->bi_error = status;
+	bio_endio(bio);
+	return BLK_QC_T_NONE;
+}
+
+// Device request handler (simple form)
 static void sbull_request(struct request_queue *queue){
   struct request *req;
   struct sbull_dev *dev;
@@ -178,11 +266,16 @@ static int __init ramdrv_init(void) {
 
   printk(KERN_NOTICE "ramdrv: spinlocked!\n");
 
-  ramdrv_sbull_dev->queue = blk_init_queue(sbull_request, &(ramdrv_sbull_dev->lock));
+  ramdrv_sbull_dev->queue = blk_alloc_queue(GFP_KERNEL);
+	if (ramdrv_sbull_dev->queue == NULL)
+		goto vfree_out;
+	blk_queue_make_request(ramdrv_sbull_dev->queue, sbull_make_request);
+
+  /*ramdrv_sbull_dev->queue = blk_init_queue(sbull_request, &(ramdrv_sbull_dev->lock));
   if (!(ramdrv_sbull_dev->queue)){
     printk(KERN_WARNING "ramdrv: unable to intialize request queue\n");
     goto vfree_out;
-  }
+  }*/
   //blk_queue_hardsect_size(ramdrv_sbull_dev->queue, logical_block_size);
   blk_queue_logical_block_size(ramdrv_sbull_dev->queue, KERNEL_SECTOR_SIZE);
 	ramdrv_sbull_dev->queue->queuedata = ramdrv_sbull_dev;
